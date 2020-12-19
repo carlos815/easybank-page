cc83e040c61c487ea8bbad50c092f1829d215348
Carlos Hernandez <carlos@cleancommit.io>
2020-12-19 09:49:36 -0400

    adjusted the styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,12 +73,13 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     }, // must be after other CSS plugins
-    /* {
+    {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
         //purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
-    }, */ 'gatsby-plugin-netlify', // make sure to keep it last in the array
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
